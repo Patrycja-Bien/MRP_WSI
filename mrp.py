b68db3357054c07a3e3023e6218ce6eb9b9450de
unknown <232039@student.uek.krakow.pl>
2024-04-29 22:53:41 +0200

    2024-04-29 MRP (improved descriptions)

--- mrp.py
+++ mrp.py
@@ -1,52 +1,68 @@
-pen_gross_requirements = [450,630]
-#pen_scheduled_receipts = 0 # chyba niepotrzebne
-pen_available = 100
-#pen_net_requirements = 0
-pen_planned_order_receipts = 0
-pen_planned_order_release = [5,6]
+pen_gross_requirements = [450,630,100]
+pen_available = 700
+pen_planned_order_receipts = [5,6,7]
 pen_lead_time = 1
-pen_safety_stock = 0
-pen_batches = 200
+pen_safety_stock = 200
+pen_batches = 100
 
-def pen_mrp(gross_requirements, planned_order_release, lead_time, in_stock, safety_stock, batches):
+def pen_mrp(gross_requirements, planned_order_receipts, lead_time, in_stock, safety_stock, batches):
     messages = []
     for i in range(len(gross_requirements)):
         if safety_stock == 0 and batches == 0:
-            week = planned_order_release[i] - lead_time
+            week = planned_order_receipts[i] - lead_time
             net = gross_requirements[i] - in_stock
             if gross_requirements[i] > in_stock:
-                messages.append(f"Jeśli na tydzień nr {planned_order_release[i]} jest potrzebne {gross_requirements[i]} długopisów,\nczas wytworzenia to {lead_time} tyg.,\nna magazynie jest {in_stock} gotowych długopisów,\nnie ma minimalnej ilości długopisów na magazynie,\ni nie ma określonej partii produkcji,\nto należy wytworzyć {net} długopisów w tygodniu nr {week}.\n" )
+                messages.append(f"Planned order receipts: {planned_order_receipts[i]}\nGross requirements: {gross_requirements[i]}\nLead Time: {lead_time} week(s)\nAvailable in stock: {in_stock}\nSafety stock: {safety_stock}\nBatches of production: {batches}\nNet requirements: {net}\nPlanned order release: {week}\n")
                 in_stock = 0
             else:
-                messages.append(f"Jeśli na tydzień nr {planned_order_release[i]} jest potrzebne {gross_requirements[i]} długopisów,\nczas wytworzenia to {lead_time} tyg.\nna magazynie jest {in_stock} gotowych długopisów,\nnie ma minimalnej ilości długopisów na magazynie,\ni nie ma określonej partii produkcji,\nto nie potrzeba wytwarzać więcej długopisów w tygodniu nr {week}.\n") 
-                in_stock = net*(-1)
+                if net <= 0:
+                    net = 0
+                messages.append(f"Planned order receipts: {planned_order_receipts[i]}\nGross requirements: {gross_requirements[i]}\nLead Time: {lead_time} week(s)\nAvailable in stock: {in_stock}\nSafety stock: {safety_stock}\nBatches of production: {batches}\nNet requirements: {net}\nPlanned order release: {week}\n") 
+                in_stock = in_stock - gross_requirements[i]
         elif safety_stock != 0 and batches == 0:
-            week = planned_order_release[i] - lead_time
+            week = planned_order_receipts[i] - lead_time
             net = gross_requirements[i] - in_stock
             if (gross_requirements[i] + safety_stock) > in_stock:
-                messages.append(f"Jeśli na tydzień nr {planned_order_release[i]} jest potrzebne {gross_requirements[i]} długopisów,\nczas wytworzenia to {lead_time} tyg.,\nna magazynie jest {in_stock} gotowych długopisów,\nminimalna ilość długopisów na magazynie to {safety_stock},\ni nie określonej partii produkcji,\nto należy wytworzyć {net + safety_stock} długopisów w tygodniu nr {week}.\n" )
+                messages.append(f"Planned order receipts: {planned_order_receipts[i]}\nGross requirements: {gross_requirements[i]}\nLead Time: {lead_time} week(s)\nAvailable in stock: {in_stock}\nSafety stock: {safety_stock}\nBatches of production: {batches}\nNet requirements: {net}\nPlanned order release: {week}\n")
                 in_stock = safety_stock
             else:
-                messages.append(f"Jeśli na tydzień nr {planned_order_release[i]} jest potrzebne {gross_requirements[i]} długopisów,\nczas wytworzenia to {lead_time} tyg.,\nna magazynie jest {in_stock} gotowych długopisów,\nminimalna ilość długopisów na magazynie to {safety_stock},\ni nie określonej partii produkcji,\nto nie potrzeba wytwarzać więcej długopisów w tygodniu nr {week}.\n") 
+                messages.append(f"Planned order receipts: {planned_order_receipts[i]}\nGross requirements: {gross_requirements[i]}\nLead Time: {lead_time} week(s)\nAvailable in stock: {in_stock}\nSafety stock: {safety_stock}\nBatches of production: {batches}\nNet requirements: {net}\nPlanned order release: {week}\n") 
                 in_stock = in_stock - gross_requirements[i]
-                if in_stock < safety_stock:
-                    sub = safety_stock - in_stock
-                    in_stock += sub
         elif safety_stock == 0 and batches != 0:
-            week = planned_order_release[i] - lead_time
-            net = gross_requirements[i] - in_stock
-            # dokończyć
-            if gross_requirements[i] > in_stock:
-                messages.append(f"Jeśli na tydzień nr {planned_order_release[i]} jest potrzebne {gross_requirements[i]} długopisów,\nczas wytworzenia to {lead_time} tyg.,\nna magazynie jest {in_stock} gotowych długopisów,\nnie ma minimalnej ilości długopisów na magazynie,\na określona partia produkcji to {batches},\nto należy wytworzyć {net} długopisów w tygodniu nr {week}.\n" )
-                in_stock = None
-            else:
-                messages.append(f"Jeśli na tydzień nr {planned_order_release[i]} jest potrzebne {gross_requirements[i]} długopisów,\nczas wytworzenia to {lead_time} tyg.\nna magazynie jest {in_stock} gotowych długopisów,\nnie ma minimalnej ilości długopisów na magazynie,\na określona partia produkcji to {batches},\nto nie potrzeba wytwarzać więcej długopisów w tygodniu nr {week}.\n") 
-                in_stock = in_stock - gross_requirements[i]
-        else:
-            pass
+        #     week = planned_order_receipts[i] - lead_time
+        #     net = gross_requirements[i] - in_stock
+        #     n = 1
+        #     net_with_batches = 0
+        #     while batches%net != 0 and net_with_batches < net:
+        #         net_with_batches = batches * n 
+        #         n += 1
+        #     if gross_requirements[i] > in_stock:
+        #         messages.append(f"Planned order receipts: {planned_order_receipts[i]}\nGross requirements: {gross_requirements[i]}\nLead Time: {lead_time} week(s)\nAvailable in stock: {in_stock}\nSafety stock: {safety_stock}\nBatches of production: {batches}\nNet requirements: {net_with_batches}\nPlanned order release: {week}\n")
+        #         in_stock = (net_with_batches + in_stock) - gross_requirements[i]
+        #     else:
+        #         messages.append(f"Planned order receipts: {planned_order_receipts[i]}\nGross requirements: {gross_requirements[i]}\nLead Time: {lead_time} week(s)\nAvailable in stock: {in_stock}\nSafety stock: {safety_stock}\nBatches of production: {batches}\nNet requirements: {net_with_batches}\nPlanned order release: {week}\n") 
+        #         in_stock = in_stock - gross_requirements[i]
+        # else:
+        #     week = planned_order_receipts[i] - lead_time
+        #     net = gross_requirements[i] - in_stock
+        #     n = 1
+        #     net_with_batches = 0
+        #     while batches%net != 0 and net_with_batches < net:
+        #         net_with_batches = batches * n 
+        #         n += 1
+        #     if (gross_requirements[i] + safety_stock) > in_stock:
+        #         messages.append(f"Planned order receipts: {planned_order_receipts[i]}\nGross requirements: {gross_requirements[i]}\nLead Time: {lead_time} week(s)\nAvailable in stock: {in_stock}\nSafety stock: {safety_stock}\nBatches of production: {batches}\nNet requirements: {net_with_batches}\nPlanned order release: {week}\n")
+        #         in_stock = (net_with_batches + in_stock + safety_stock) - gross_requirements[i]
+        #         if in_stock < safety_stock:
+        #             raise ValueError
+        #     else:
+        #         messages.append(f"Planned order receipts: {planned_order_receipts[i]}\nGross requirements: {gross_requirements[i]}\nLead Time: {lead_time} week(s)\nAvailable in stock: {in_stock}\nSafety stock: {safety_stock}\nBatches of production: {batches}\nNet requirements: {net_with_batches}\nPlanned order release: {week}\n") 
+        #         if in_stock < safety_stock:
+        #             raise ValueError
+        #         in_stock = in_stock - gross_requirements[i]
     return "\n".join(map(str, messages))
 
-print(pen_mrp(pen_gross_requirements, pen_planned_order_release, pen_lead_time, pen_available, pen_safety_stock, pen_batches))
+print(pen_mrp(pen_gross_requirements, pen_planned_order_receipts, pen_lead_time, pen_available, pen_safety_stock, pen_batches))
 
 body_gross_requirements = ""
 body_scheduled_requests = ""
